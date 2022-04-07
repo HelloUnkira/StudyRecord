4789f416792d6d530bc9aefa0aa42873de7e5da8
HelloUnkira <15197794498@139.com>
2022-04-07 23:57:01 +0800

    .

--- BasicKnowledge/A_C&C++/C_Math/Cflint/CflintResidueClass.c
+++ BasicKnowledge/A_C&C++/C_Math/Cflint/CflintResidueClass.c
@@ -6,6 +6,59 @@
 /*****************************************************************************/
 /*****************************************************************************/
 /*****************************************************************************/
+void Cflint_GCD(CFLINT_TYPE *A, CFLINT_TYPE *B, CFLINT_TYPE *X, CFLINT_TYPE *Y,
+                CFLINT_TYPE *Result, CFLINT_TYPE *Quotient, CFLINT_TYPE *Module,
+                CFLINT_TYPE *Operand0, CFLINT_TYPE *Operand1, CFLINT_TYPE *Temp,
+                uint32_t Length)
+{
+    /* 初始化:X=1,Y=0 */
+    Cflint_SetValue(Y, Length, 0);
+    Cflint_SetValue(X, Length, 0);
+    Cflint_AdditionBit(X, Length, 1);
+    /* 当A或B为0时 */
+    if (Cflint_IsZero(B, Length) == true)
+        return;
+    /* 将A,B拷贝到缓冲区,数据计算在缓冲区内 */
+    Cflint_Copy(Operand0, A, Length);
+    Cflint_Copy(Operand1, B, Length);
+    /* 先进行向下递归 */
+    bool Cflint_GCD1_Run = true;
+    while (Cflint_GCD1_Run) {
+        /* Quotient=Operand0/Operand1,Module=Operand0%Operand1 */
+        Cflint_Devide(Quotient, Module, Operand0, Operand1, Length);
+        /* 当模为0的时候结束,用于回溯递归 */
+        if (Cflint_IsZero(Module, Length) == true)
+            Cflint_GCD1_Run = false;
+        /* 更新被除数和除数 */
+        Cflint_Copy(Operand0, Operand1, Length);
+        Cflint_Copy(Operand1, Module, Length);
+    }
+    /* 再进行向上回溯 */
+    bool Cflint_GCD2_Run = true;
+    while (Cflint_GCD2_Run) {
+        /* 先利用上一级的商和本级的被除数求出上一级被除数 */
+        Cflint_Multiply(Temp, Quotient, Operand0, Length);
+        Cflint_Addition(Temp, Temp, Operand1, Length);
+        /* 还原上一级的除数和被除数 */
+        Cflint_Copy(Operand1, Operand0, Length);
+        Cflint_Copy(Operand0, Temp, Length);
+        /* 使用商去更新X和Y */
+        Cflint_Swap(X, Y, Length);
+        /* 因为是无符号数字,所以直接求其绝对值数,因为它一定是负数 */
+        Cflint_SubtractionBit(Quotient, Length, 1);
+        Cflint_Multiply(Temp, Quotient, X, Length);
+        Cflint_Copy(Y, Temp, Length);
+        /* 如果达到退出条件,退出 */
+        if (Cflint_Equal(A, Operand0, Length) == 1 &&
+            Cflint_Equal(B, Operand1, Length) == 1)
+            Cflint_GCD2_Run = false;
+        /* 重新更新商和模 */
+        Cflint_Devide(Quotient, Module, Operand0, Operand1, Length);
+    }
+}
+/*****************************************************************************/
+/*****************************************************************************/
+/*****************************************************************************/
 /* 蒙哥马利约减 */
 /* T,Temp1,Temp2长度为Length*2 */
 void Cflint_M_ModuloReduction(CFLINT_TYPE *Result, uint32_t Length, int64_t R,
