ec230f82214d01f4698ab0a62626b85dcc108899
HelloUnkira <15197794498@139.com>
2022-07-25 19:38:44 +0800

    ...

--- KnowledgeSet/B_StudyRecord/Python/Spider/Spider_requests.py
+++ KnowledgeSet/B_StudyRecord/Python/Spider/Spider_requests.py
@@ -151,6 +151,100 @@ if __name__ == '__main__':
 '''
 
 
+'''
+# 下载91看剧资源参考案例,电影和电视流程一致
+import requests
+import asyncio
+import aiohttp
+import aiofiles
+import functools
+import re
+import os
+import shutil
+from Crypto.Cipher import AES
 
 
+def ts_name_compare(ts_name1, ts_name2):
+    match1 = re.findall('./spider_dl_tt_temp/(.*?).ts', ts_name1)
+    match2 = re.findall('./spider_dl_tt_temp/(.*?).ts', ts_name2)
+    if int(match1[0]) < int(match2[0]):
+        return -1
+    if int(match1[0]) > int(match2[0]):
+        return +1
+    return 0
 
+
+async def frame_download_and_dump(index, url, key_value, session):
+    aes = AES.new(key=key_value, IV=b"0000000000000000", mode=AES.MODE_CBC)
+    async with session.get(url) as response:
+        assert response.status == 200
+        response.encoding = 'utf-8'
+        async with aiofiles.open('./spider_dl_tt_temp/%d.ts' % index, mode='wb') as file:
+            await file.write(aes.decrypt(await response.content.read()))
+
+
+async def main():
+    # Step1: 从起始源获得页面
+    url = 'https://91kju.com/vod-play-id-8482-sid-1-pid-1.html'     # 杀戒(2022-07-25资源不稳定)
+    # url = 'https://91kju.com/vod-play-id-63237-sid-1-pid-1.html'  # 齐天大圣(2022-07-25资源不稳定)
+    with requests.get(url) as response:
+        assert response.status_code == 200
+        response.encoding = 'utf-8'
+        # 提取转接头
+        adaptor_url = re.findall('cms_player(.*?)"url":"(.*?)"', str(response.text))[0][1].replace('\\', '')
+        print(adaptor_url)
+    with requests.get(adaptor_url) as response:
+        assert response.status_code == 200
+        response.encoding = 'utf-8'
+        # 提取第一级m3u8
+        m3u8_url = response.url
+        print(m3u8_url)
+    # Step2: 从一级界面提取二级界面
+    with requests.get(m3u8_url) as response:
+        assert response.status_code == 200
+        response.encoding = 'utf-8'
+        # 提取第二级m3u8
+        m3u8_url = re.findall('(.*?)/[0-9](.&?)', m3u8_url)[0][0] + str(response.text).split('\n')[2]
+        print(m3u8_url)
+    # Step3: 从二级界面提取Key界面和jpg_list界面
+    with requests.get(m3u8_url) as response:
+        assert response.status_code == 200
+        response.encoding = 'utf-8'
+        # 提取key
+        key_url = re.findall('URI="(.*?)"(.*?)', response.text)[0][0]
+        print(key_url)
+        # 提取ts列表,这里都是.jpg格式的文件,需要移除开头的一部分格式头
+        jpg_url_lst = [(index, http) for index, http in enumerate(re.findall(',\n(.*?)\n', response.text))]
+        # print(jpg_url_lst)
+    # Step4: 从Key界面提取加密秘钥
+    with requests.get(key_url) as response:
+        assert response.status_code == 200
+        response.encoding = 'utf-8'
+        # 提取key值
+        key_value = response.content
+        print(key_value)
+    # Step5: 创建一个相对路径文件夹,如果它不存在的话,否则先移除再创建
+    if os.path.isdir('./spider_dl_tt_temp'):
+        shutil.rmtree('./spider_dl_tt_temp')
+    if not os.path.isdir('./spider_dl_tt_temp'):
+        os.mkdir('./spider_dl_tt_temp')
+    # Step6: 创建异步协程任务集并且等待任务集执行完毕
+    async with aiohttp.ClientSession() as session:
+        await asyncio.wait([asyncio.create_task(frame_download_and_dump(item[0], item[1], key_value, session))
+                            for item in jpg_url_lst])
+    # Step7: 从名字切取特质标识,提取临时文件,并进行组包,注意要对其排序,否则为错误的顺序
+    ts_list = ['./spider_dl_tt_temp/%d.ts' % item[0] for item in jpg_url_lst]
+    ts_list.sort(key=functools.cmp_to_key(ts_name_compare))
+    # 创建目标文件,如果它不存在的话
+    if not os.path.isfile(' ./%s.mp4' % url.rsplit('/', 1)[1].split('.')[0]):
+        open(' ./%s.mp4' % url.rsplit('/', 1)[1].split('.')[0], mode='wb').close()
+    os.system('copy /b ' + ' '.join(ts_list) + ' ./%s.mp4' % url.rsplit('/', 1)[1].split('.')[0])
+    print('copy /b ' + ' '.join(ts_list) + ' ./%s.mp4' % url.rsplit('/', 1)[1].split('.')[0])
+    # Step x: 移除临时文件夹及其临时文件
+    if os.path.isdir('./spider_dl_tt_temp'):
+        shutil.rmtree('./spider_dl_tt_temp')
+    print('--over--')
+
+if __name__ == '__main__':
+    asyncio.run(main())
+'''
