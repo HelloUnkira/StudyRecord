1ff861e77ff0b9fa3ed88cde591358149d2bfb11
HelloUnkira <15197794498@139.com>
2022-08-03 21:40:00 +0800

    ...

---
+++ KnowledgeSet/B_StudyRecord/Python/Spider/Spider_scrapy.py
@@ -0,0 +1,4 @@
+
+# I Don't Need To Use This Framework, At Least For Now.
+# So, The Only Thing I Should Do Is Learn A Little Bit More When I Need It.
+# That's Easy For Me, But Now, I Drunk, Skip...
