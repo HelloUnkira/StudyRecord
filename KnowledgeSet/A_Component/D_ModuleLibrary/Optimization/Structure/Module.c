df54170eaa9db8cbecabd19fbc0d5a27f21e6339
HelloUnkira <15197794498@139.com>
2022-08-09 22:59:16 +0800

    ...

---
+++ KnowledgeSet/A_Component/D_ModuleLibrary/Optimization/Structure/Module.c
@@ -0,0 +1,24 @@
+
+/*=====[Inclusions of function dependencies]=================================*/
+
+/*=====[Definition macros of private constants]==============================*/
+
+/*=====[Definitions of extern global variables]==============================*/
+
+/*=====[Definitions of public global variables]==============================*/
+
+/*=====[Definitions of private global variables]=============================*/
+
+/*=====[Main function, program entry point after power on or reset]==========*/
+
+int main( void )
+{
+   // ----- Setup -----------------------------------
+
+   // ----- Repeat for ever -------------------------
+
+   // YOU NEVER REACH HERE, because this program runs directly or on a
+   // microcontroller and is not called by any Operating System, as in the 
+   // case of a PC program.
+   return 0;
+}
---
+++ KnowledgeSet/A_Component/D_ModuleLibrary/Optimization/Structure/Module.h
@@ -0,0 +1,20 @@
+/*=====[Avoid multiple inclusion - begin]====================================*/
+
+#ifndef __MODULE_H__
+#define __MODULE_H__
+
+/*=====[Inclusions of public function dependencies]==========================*/
+
+/*=====[Definition macros of public constants]===============================*/
+
+/*=====[Public function-like macros]=========================================*/
+
+/*=====[Definitions of public data types]====================================*/
+
+/*=====[Prototypes (declarations) of public functions]=======================*/
+
+/*=====[Prototypes (declarations) of public interrupt functions]=============*/
+
+/*=====[Avoid multiple inclusion - end]======================================*/
+
+#endif /* __MODULE_H__ */
