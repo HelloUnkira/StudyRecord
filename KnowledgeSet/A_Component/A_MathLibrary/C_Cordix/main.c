d3721764baebdecb06d96289b92ff3a570477749
HelloUnkira <99794704+HelloUnkira@users.noreply.github.com>
2023-06-16 03:00:38 +0800

    ...

--- KnowledgeSet/A_Component/A_MathLibrary/C_Cordix/main.c
+++ KnowledgeSet/A_Component/A_MathLibrary/C_Cordix/main.c
@@ -6,9 +6,9 @@ int main(int argc, char *argv[]) {
     
     for (int i = 0; i < 10; i++) {//测试arctan,sin,cos,tan
         double t = (double)i * 1.1;
-        double Sin =  CordixSin(t);
-        double Cos =  CordixCos(t);
-        double Tan =  CordixTan(t);
+        double Sin  = CordixSin(t);
+        double Cos  = CordixCos(t);
+        double Tan  = CordixTan(t);
         double ATan = CordixArctan(Sin, Cos);
         printf("\n\n");
         printf("CordixSin(%lf) = %.10lf, sin(%lf) = %.10lf\n", t, Sin, t, sin(t));
